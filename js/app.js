3122b57de3eb859208e5f4280e2159e0fa3f4961
Guilherme Oliveira <guibalbinos@gmail.com>
2025-05-20 20:12:05 -0300

    Arrumando carrinho

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
 function adicionar() {
-    let nomeProduto = document.getElementById('produto');
-    let quantidade = document.getElementById('quantidade');
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+
+    //Calcular o valor e subtotal
+    let valorTotal = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+
+    //Adicionar produto ao carrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul"> ${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorTotal}</span>
+        </section>`;
+
+    //Atualizar o valor total
     
+
 }
 
 function limpar() {
